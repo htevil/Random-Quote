73dafddc8f360b95372a975033e7fa1a138c340d
Harshit Tiwari <htevil>
2024-07-14 12:42:32 +0530

    fivetheen commit

--- server/server.mjs
+++ server/server.mjs
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import express from 'express';
 import cors from 'cors';
 
@@ -10,17 +9,17 @@ app.use(cors());
 
 // Define a route to fetch data from the API and return it
 app.get('/api/quote', async (req, res) => {
-    try {
-        const response = await fetch('https://favqs.com/api/qotd');
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Error fetching quote:', error);
-        res.status(500).json({ error: 'Failed to fetch quote' });
-    }
+  try {
+    const response = await fetch('https://favqs.com/api/qotd');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    res.status(500).json({ error: 'Failed to fetch quote' });
+  }
 });
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
