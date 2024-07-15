3ed55106d79cf20fedfccb5901eeaedd4f256845
Harshit Tiwari <htevil>
2024-07-15 10:48:51 +0530

    seventheen commit

--- server/server.mjs
+++ server/server.mjs
@@ -4,24 +4,12 @@ import cors from 'cors';
 import axios from 'axios';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Use CORS middleware
 app.use(cors());
 
 // Define a route to fetch data from the favqs API and return it
-app.get('/api/quote', async (req, res) => {
-    try {
-        const response = await fetch('https://favqs.com/api/qotd');
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Error fetching quote:', error);
-        res.status(500).json({ error: 'Failed to fetch quote' });
-    }
-});
-
-// Define a route to fetch data from the mocky API and return it
 app.get('/api/Quote', async (req, res) => {
     const options = {
         method: 'GET',
@@ -31,11 +19,26 @@ app.get('/api/Quote', async (req, res) => {
         const response = await axios.request(options);
         res.json(response.data);
     } catch (error) {
-      console.error('Error fetching quote:', error);
-      res.status(500).json({ error: 'Failed to fetch quote' });
-  }
+        console.error('Error fetching quote:', error);
+        res.status(500).json({ error: 'Failed to fetch quote' });
+    }
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+// Gracefully shut down the server
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
 });
